69b966724df0af0af72d83fc295f4189d483b938
chichi <cyannara@naver.com>
2023-11-29 09:10:24 +0900

    emp 쿼리 수정

--- myserver/src/main/java/com/yedam/myserver/emp/web/EmployeeController.java
+++ myserver/src/main/java/com/yedam/myserver/emp/web/EmployeeController.java
@@ -9,37 +9,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yedam.myserver.emp.mapper.EmployeeMapper;
-import com.yedam.myserver.emp.vo.Departments;
 import com.yedam.myserver.emp.vo.Employee;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*",  maxAge = 3600)
 public class EmployeeController {
-	private static final Logger logger = 
-			LoggerFactory.getLogger(EmployeeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 	
 	@Autowired
 	private EmployeeMapper employeeDao;
 	
 	//사원검색
-	@RequestMapping(value="/empSelect")
+	@GetMapping(value="/empSelect")
 	public List<Employee> selectDepartment() {
 		return employeeDao.findEmployees();
 	}	
 	
 	//사원 ID검색
-	@RequestMapping(value="/empId")
+	@GetMapping(value="/empId")
 	public Employee findById(Employee emp) {
 		return employeeDao.findById(emp);
 	}
 	
 	//부서와 직업 검색
-	@RequestMapping(value="/empDeptJob")
+	@GetMapping(value="/empDeptJob")
 	public Map<String, Object> jobDeptList() {
 		Map<String, Object> map = new HashMap<>();
 		map.put("jobs", employeeDao.findJobs());
@@ -48,21 +46,21 @@ public class EmployeeController {
 	}	
 	
 	//사원등록
-	@RequestMapping(value="/empInsert", method=RequestMethod.POST )
+	@PostMapping(value="/empInsert")
 	public  Employee insertEmployees(Employee emp, HttpServletResponse response) {
 		employeeDao.persist(emp);
 		return emp;
 	}
 	
 	//사원정보수정
-	@RequestMapping(value="/empUpdate", method=RequestMethod.POST )
+	@PostMapping(value="/empUpdate" )
 	public  Employee updateEmployees(Employee emp, HttpServletResponse response) {
 		employeeDao.merge(emp);
 		return emp;
 	}	
 	
 	//사원삭제
-	@RequestMapping(value="/empDelete", method=RequestMethod.POST )
+	@PostMapping(value="/empDelete")
 	public  Employee deleteEmployees(Employee emp, HttpServletResponse response) {
 		employeeDao.remove(emp);
 		return emp;
